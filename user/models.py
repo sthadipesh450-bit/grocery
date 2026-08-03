16fadc3167c0cff547aa7a63e8f1a90f63fc179b
sthadipesh450-bit <sthadipesh450@gmail.com>
2026-08-03 11:36:23 +0545

    changes in models and admin

--- user/models.py
+++ user/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    class Meta:
+        db_table = 'roles'
+        verbose_name = 'Role'
+        verbose_name_plural = "Roles"
+    def __str__(self):
+        return self.name
+
 # Create your models here.
 class User(models.Model):
     id = models.AutoField(primary_key=True)
@@ -10,6 +19,13 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+    role = models.ForeignKey(
+        Role,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        default=None,
+    )
 
     class Meta:
         db_table = 'users'
